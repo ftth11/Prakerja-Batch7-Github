c793b797d665dc91ec7cf38a8270b18a934cd46c
ftth11 <136133364+ftth11@users.noreply.github.com>
2023-06-20 13:33:27 +0700

    Add files via upload

---
+++ Sesi 5/JS/script.js
@@ -0,0 +1,8 @@
+// menampilkan text di alert
+alert("Hello from alert");
+
+// menampilkan text di confirm
+confirm ("Hello from confirm");
+
+// menampilkan text di prompt
+prompt ("Hello from prompt");
